1e3890e6736e536935a792abb5002ca97ad5c9e4
HarshDev56 <harshfichadiya99@gmail.com>
2023-06-08 09:19:24 +0530

    add example env file

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -10,7 +10,6 @@ const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
